778fc79b9d9c0ac7d14a9f983e827ccc19e98889
flavio-ss <9090flavio@gmail.com>
2021-09-01 21:20:47 -0300

    GRPC - IMPLEMENTANDO SERVER STREAM

--- services/user.go
+++ services/user.go
@@ -3,6 +3,7 @@ package services
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/flaviossantana/go-grpc/pb"
 )
@@ -26,4 +27,45 @@ func (*UserService) AddUser(ctx context.Context, req *pb.User) (*pb.User, error)
 		Email: req.GetEmail(),
 	}, nil
 
+}
+
+func (*UserService) AddUserVerbose(req *pb.User, stream pb.UserService_AddUserVerboseServer) error {
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Init",
+		User:   &pb.User{},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Inserindo",
+		User:   &pb.User{},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Usuário foi inserido",
+		User: &pb.User{
+			Id:    "8970",
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Finalizado!",
+		User: &pb.User{
+			Id:    "8970",
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	return nil
 }
